4fcb9fde2318f638a9f7fb27b56ac275d5cdf1c7
Aidan Fitzgerald <aidan-fitz@users.noreply.github.com>
2014-11-13 12:31:23 -0500

    Oops, that duplicates everything

--- 02ArrayList/ArrayListMethods.java
+++ 02ArrayList/ArrayListMethods.java
@@ -4,17 +4,17 @@ class ArrayListMethods {
 
     public static void collapseDuplicates(List list) {
         for (int i = 0; i < list.size() - 1; i++) {
-	        while (i < list.size() - 1 && list.get(i).equals(list.get(i+1)) ) {
-		        list.remove(i+1);
-	        }
+	    while (i < list.size() - 1 && list.get(i).equals(list.get(i+1)) ) {
+	        list.remove(i+1);
 	    }
+	}
     }
     
     Random izer = new Random();
     
     public static void randomize(List list) {
         for (int i = 0; i < list.size(); i++) {
-            list.add(izer.nextInt(list.size()), list.get(0));
+            list.add(izer.nextInt(list.size()), list.remove(0));
         }
     }
 
