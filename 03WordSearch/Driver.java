c266af133a9c9fede25bad34949febeec4b187c3
aidan-fitz <duckjr159@gmail.com>
2014-11-24 17:55:17 -0500

    new Driver for my hotrod

---
+++ 03WordSearch/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+
+    public static String[] akameWords = ["AKAME", "MURASAME", "MINE", "PUMPKIN", "TEIGU", "TATSUMI", "NIGHTRAID", "INCURSIO", "KYOROCH", "SHEELE", "CHELSEA", "NAJENDA", "ESDEATH", "JAEGARS", "SERYU", "WAVE", "LEONE"];
+
+    public static void main(String[] args) {
+	
+    }
+}
--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -21,9 +21,8 @@ public class WordGrid {
 	}
     }
 
-    /**The proper formatting for a WordGrid is created in the toString.
-     *@return a String with each character separated by spaces, and each row
-     *separated by newlines.
+    /**
+     * @return a string with columns separated by spaces and rows separated by newlines.
      */
     public String toString() {
 	StringBuilder swag = new StringBuilder();
@@ -31,32 +30,17 @@ public class WordGrid {
 	    for (char cell: row) {
 		swag.append(cell).append(' ');
 	    }
-	    // Replace the last space with two line breaks
-	    swag.replace(swag.length() - 1, swag.length(), "\n\n");
+	    // Replace the last space with a line break
+	    swag.replace(swag.length() - 1, swag.length(), '\n');
 	}
-	// Delete the two newlines at the end
-	swag.delete(swag.length() - 2, swag.length());
 	// ok, swag
 	return swag.toString();
     }
 
-    /**Attempts to add a given word to the specified position of the WordGrid.
-     *The word is added from left to right, must fit on the WordGrid, and must
-     *have a corresponding letter to match any letters that it overlaps.
-     *
-     *@param word is any text to be added to the word grid.
-     *@param row is the vertical locaiton of where you want the word to start.
-     *@param col is the horizontal location of where you want the word to start.
-     *@return true when the word is added successfully. When the word doesn't fit,
-     *or there are overlapping letters that do not match, then false is returned.
-     */
-    public boolean addWordHorizontal(String word,int row, int col){
-    }
-
     /**
-     * Checks that a word will fit into the grid starting at the specified {@code row} and {@code col} and going in the specified {@code direction}.
+     * Attempts to add a given word to the grid starting at the specified {@code row} and {@code col} and going in the specified {@code direction}.
      * The {@code direction} is an integer that specifies one of eight directions in the grid.
-     * The directions go clockwise from 0, which is straight up, to 7, which is diagonally up and to the left.
+     * The directions go clockwise from 0, which is straight up, to 7, which is up and to the left.
      *
      * @param word the word to be checked
      * @param row the vertical coordinate of the starting position
@@ -64,28 +48,37 @@ public class WordGrid {
      * @param direction an integer from 0 to 7 that specifies the direction in which to add letters
      * @return {@code true} if the word can be inserted here, {@code false} otherwise
      */
-    public boolean wordFits(String word, int row, int col, int direction) {
+    public boolean add(String word, int row, int col, int direction) {
 	try {
-	    for (int index = 0; index < word.length; index++) {
+	    for (int index = 0; index < word.length(); index++) {
 		char inGrid = charAt(index, row, col, direction);
     		if (inGrid != ' ' && inGrid != word.charAt(index))) {
-		    // not a space and not equal to the corresponding character
-		    // in the word
-		    return false;
-		}
+		// not a space and not equal to the corresponding character
+		// in the word
+		return false;
 	    }
 	}
 	catch (ArrayIndexOutOfBoundsException e) {
 	    // went past the boundaries of the grid, so the word doesn't fit
 	    return false;
 	}
-	// match
-        return true;
+	// match - eat it, just eat it
+	for (int index = 0; index < word.length(); index++) {
+	    addChar(c, index, row, col, direction);
+	}
+	return true;
     }
 
     private char charAt(int index, int row, int col, int direction) {
-	return data[row + rowIncr(direction)][col + colIncr(direction)];
+	return data[row + index*rowIncr(direction)][col + index*colIncr(direction)];
     }
+
+    /* WARNING: Does not check first. Be sure you won't f*** up the grid. */
+    private void addChar(char c, int index, int row, int col, int direction) {
+	data[row + index*rowIncr(direction)][col + index*colIncr(direction)] = c;
+    }
+
+    /* +1 is down, -1 is up */
     private int rowIncr(int direction) {
 	switch (direction) {
 	case 7: case 0: case 1:  return -1; // go up
@@ -95,6 +88,8 @@ public class WordGrid {
 	    throw new IllegalArgumentException("direction must be in [0, 8)");
 	}
     }
+
+    /* +1 is right, -1 is left */
     private int colIncr(int direction) {
 	switch (direction) {
 	case 1: case 2: case 3:  return 1; // go right
@@ -104,7 +99,5 @@ public class WordGrid {
 	    throw new IllegalArgumentException("direction must be in [0, 8)");
 	}
     }
-
-    //vertical + diagonal should be implemented as well.
 
 }
