9eb0d8842ad332875f1898c9b40b7cc6d06c7533
aidan-fitz <duckjr159@gmail.com>
2014-12-08 12:05:12 -0500

    Let's objectify humans

---
+++ humor/Human.java
@@ -0,0 +1,12 @@
+/**
+ * Objectifying human beings
+ */
+public class Human {
+
+    /**
+     * Oh yeah, all humans are supposed to be equal
+     */
+    public boolean equals(Human other) {
+	return true;
+    }
+}
